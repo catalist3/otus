a278ea5011519d742928d61199da23875e1ee26b
dimon <dimon-otus>
2023-07-29 23:17:04 +0300

    Add task5nfs files and report

---
+++ task5nfs/clientnfs.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+yum install nfs-utils
+systemctl enable firewalld --now 
+systemctl status firewalld
+echo "192.168.10.70:/srv/share/ /mnt nfs vers=3,proto=udp,noauto,x-systemd.automount 0 0" >> /etc/fstab
+
+systemctl daemon-reload 
+systemctl restart remote-fs.target
